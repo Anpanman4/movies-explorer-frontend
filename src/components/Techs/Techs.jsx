7970cb4156e2f249d9f7b18e1a251fe1b5f75fbd
Aleksey Kondratev <Anpanman4@yandex.ru>
2023-05-27 11:17:53 +0300

    feat: do techs

---
+++ src/components/Techs/Techs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import './Techs.css'
+
+function Techs() {
+  return (
+    <>
+      <section className="techs">
+        <h2 className="techs__head">Технологии</h2>
+        <h3 className="techs__title">7 технологий</h3>
+        <p className="techs__subtitle">
+          На курсе веб-разработки мы освоили технологии,
+          которые применили в дипломном проекте.
+        </p>
+        <ul className="techs__skills">
+          <li className="techs__skill">HTML</li>
+          <li className="techs__skill">CSS</li>
+          <li className="techs__skill">JS</li>
+          <li className="techs__skill">React</li>
+          <li className="techs__skill">Git</li>
+          <li className="techs__skill">Express.js</li>
+          <li className="techs__skill">MongoDB</li>
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default Techs;
